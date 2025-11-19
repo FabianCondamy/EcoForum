704c0a547ef04369bf63f08f0ad5e5085ee11e46
Anne-lauretho <anne-laure.thomas@etu.umontpellier.fr>
2025-11-19 13:49:15 +0100

    Mise à jour UI : ajout des blocs Explications après les graphiques

--- src/app.R
+++ src/app.R
@@ -1,6 +1,14 @@
 library(shiny)
 
+# Modules
+source("R/mod_serietemp.R")
+source("R/mod_map.R")
+source("R/mod_stats.R")
+source("R/mod_summary.R")
+
+# UI et server
 source("ui.R")
 source("server.R")
 
+# Lancer l'application
 shinyApp(ui = ui, server = server)
