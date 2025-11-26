6efe4e1be675c50a734de9818d872b591c3aa4b1
Wahelelm <welmazzouji72@gmail.com>
2025-11-26 17:06:08 +0100

    Modification app.R

--- src/app.R
+++ src/app.R
@@ -2,5 +2,10 @@ library(shiny)
 
 source("ui.R")
 source("server.R")
+source("R/mod_data.R")
+source("R/mod_map.R")
+source("R/mod_serietemp.R")
+source("R/mod_stats.R")
+source("R/mod_summary.R")
 
 shinyApp(ui = ui, server = server)
