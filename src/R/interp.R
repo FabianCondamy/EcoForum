4dc741203f557a40666c23e6c19b1644960954d1
Lucien Duigou <luluduipou@hotmail.fr>
2025-12-04 12:35:09 +0100

    modification interp.R

--- src/R/interp.R
+++ src/R/interp.R
@@ -1,39 +1,5 @@
 librarian::shelf(shiny, ggplot2, dplyr, sf, maptiles, raster, tidyterra, ggspatial, lubridate, tidyr,here,gstat,sp,automap,stars)
 
-# Chargement des données
-ref <- read.csv("C:/Users/lulud/Desktop/EcoForum/data/raw-data/temp_ref.csv", h = TRUE, sep = ",") %>%
-  group_by(X_date) %>%
-  summarise(temp.ref = mean(outside_temp)) %>%
-  mutate(date = ymd(X_date),
-         YYYY = year(date),
-         MM = month(date),
-         DD = day(date)) %>%
-  select(-X_date)
-
-temp <- read.csv("C:/Users/lulud/Desktop/EcoForum/data/derived-data/250703_corr.csv", h = TRUE, sep = ";") %>%
-  separate(coord, sep = ",", into = c("Longitude", "Latitude"))
-
-habitat <- read.csv("C:/Users/lulud/Desktop/EcoForum/data/raw-data/habitat.csv", h = TRUE, sep = ";") %>%
-  rename(sensor = id) %>%
-  select(sensor, type.zone)
-
-# Transformation spatiale et enrichissement
-temp <- st_as_sf(temp, coords = c("Latitude", "Longitude"), crs = 4326) %>%
-  st_transform("EPSG:2154") %>%
-  mutate(date.time = ymd_hms(date.time),
-         YYYY = year(date.time),
-         MM = month(date.time),
-         DD = day(date.time),
-         HH = hour(date.time),
-         Min = minute(date.time),
-         SS = second(date.time),
-         doy = yday(make_date(YYYY, MM, DD))) %>%
-  left_join(habitat, by = "sensor") %>%
-  left_join(ref, by = c("YYYY", "MM", "DD")) %>%
-  mutate(temp.ecart.raw = temp.corr - temp.ref,
-         temp.ecart.prc = (temp.corr - temp.ref) / temp.ref)
-
-temp$month_name <- factor(month.name[temp$MM], levels = month.name)
 
 batiments <- st_read("batiments/batiments.geojson")
 batiments=st_transform(batiments,3857)
